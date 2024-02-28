5e1aebe465034ea6edb20edee7d7042910cc2bd6
caio <cmb2@cin.ufpe.br>
2024-02-28 11:59:39 -0300

    Correção de alguns bugs e página inicial básica

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { GamesService } from './services/games/games.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'SelecaoVlab';
+  games: any[] = [];
+
+  constructor(private gamesService: GamesService){}
+
+  ngOnInit(){
+    this.gamesService.getAllGames().subscribe(data => {
+      this.games = data;
+      console.log("Result:", this.games);
+    });
+  }
 }
